63ad330a1025ef748fdd4ce4d166365da55dc94a
safroniaziz <safroni.aziz@gmail.com>
2021-12-05 02:14:10 +0700

    menambahkan shortcut icon

--- resources/views/layouts/layout2.blade.php
+++ resources/views/layouts/layout2.blade.php
@@ -4,6 +4,7 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <title>Opac Man IC | @yield('title')</title>
+  <link rel="shortcut icon" href="{{ asset('assets/images/manic.png') }}">
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <!-- Bootstrap 3.3.7 -->
