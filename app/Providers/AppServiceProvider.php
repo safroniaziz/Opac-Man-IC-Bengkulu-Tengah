2dac8a8f25e25d843ea268e2451377d2cd28645a
agoyunib <67571944+agoyunib@users.noreply.github.com>
2021-12-13 16:02:24 +0700

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,7 +24,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         if ($this->app->environment('production')) {
-            \URL::forceSchere('https');
+            \URL::forceScheme('https');
         }
     }
 }
